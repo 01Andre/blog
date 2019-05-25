8cf5186bcd1f3a9a30096e4bda84ffd0778124c1
Andre Dupin <dd.dupin@gmail.com>
2019-05-25 16:33:45 +0200

    ajout du ç au slugifyer

--- src/Service/Slugify.php
+++ src/Service/Slugify.php
@@ -9,8 +9,8 @@ class Slugify
 
     public function generate(string $input): string
     {
-        $forbidenLetters = ['à','é', 'è', 'ê' , ' ', '\'', '--','!',',' , ', ',' !' ];
-        $replacementLetters = ['a','e', 'e', 'e', '-', '', '-','','','',''];
+        $forbidenLetters = ['ç','à','é', 'è', 'ê' , ' ', '\'', '--','!',',' , ', ',' !' ];
+        $replacementLetters = ['c','a','e', 'e', 'e', '-', '', '-','','','',''];
 
         $input=  strtolower(str_replace($forbidenLetters, $replacementLetters,$input));
         $input= trim ( $input , $character_mask = " \t\n\r\0\x0B\-" );
